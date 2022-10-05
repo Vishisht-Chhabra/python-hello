38d1e9ae2243c9676d72952e2d23c9635ddbe44f
Vishisht-Chhabra <84404680+Vishisht-Chhabra@users.noreply.github.com>
2022-10-05 14:26:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ import os
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
-        name = "world"
-    message = "Hello, " + name + "!\n"
+        name = "World"
+    message = "Good Morning, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
