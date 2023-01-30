4b04bd47c477de4c80a5258135dbe8fed6e33e2c
Vishisht-Chhabra <84404680+Vishisht-Chhabra@users.noreply.github.com>
2023-01-30 15:53:27 +0530

    root to logger

--- server.py
+++ server.py
@@ -7,14 +7,14 @@ import os
 import logging
 import sys
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
-root.addHandler(handler)
+logger.addHandler(handler)
 ######################
 
 
