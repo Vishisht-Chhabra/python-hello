15380dfdae29c75d81ea80183496d9706370a4ae
Vishisht-Chhabra <84404680+Vishisht-Chhabra@users.noreply.github.com>
2023-01-30 15:26:38 +0530

    Logging
    enable logging

--- server.py
+++ server.py
@@ -2,13 +2,35 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
+import logging.handlers
+
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Handler 
+LOG_FILE = '/tmp/sample-app.log'
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+handler.setLevel(logging.INFO)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Add Formatter to Handler
+handler.setFormatter(formatter)
+
+# add Handler to Logger
+logger.addHandler(handler)
+
+
 
 def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "World"
     message = "Good Morning, " + name + "!\n"
-    print(message)
+    #print(message)
+    logger.info(message)
     return Response(message)
 
 if __name__ == '__main__':
